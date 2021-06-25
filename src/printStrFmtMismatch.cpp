8343627f6d1e0f51c4043337d29c9deaa11625bc
Neeraj Singh <neeraj.singh31285@gmail.com>
2021-06-25 11:25:31 +0530

    new checks

---
+++ src/printStrFmtMismatch.cpp
@@ -0,0 +1,24 @@
+// UnitT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <cstdio>
+
+void foo(const char* pc, int i, char c)
+{
+	char str[100];
+
+	sprintf(str, "%o", i, pc);	//Non-Compliant - format specifiers count does not matches the variable's count
+	sprintf(str, "%p", pc);		//Compliant - format specifiers count matches the variable's count
+}
+
+void main()
+{
+	int x = 10;
+	char ch = 'q';
+
+	foo("FormatStr", 0, ch);
+
+	printf("char : %d", ch);	//Compliant - format specifiers count matches the variable's count
+	
+	return;
+}
