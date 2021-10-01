da0e42eed090b6f93e8ac2659834f20f9df6cbea
Neeraj Singh <neeraj.singh31285@gmail.com>
2021-10-01 12:50:25 +0530

    Pr_test

---
+++ src/complex.cpp
@@ -0,0 +1,35 @@
+
+#include "inc/complex.h"
+
+Complex::Complex(double _real, double _imaginary) {
+	this->real = _real;
+	this->imaginary = _imaginary;
+}
+
+Complex::Complex(const Complex& rhs) {
+	this->real = rhs.real; 
+	this->imaginary = rhs.imaginary;
+}
+
+Complex::~Complex() {
+
+}
+
+Complex& Complex::operator=(const Complex& rhs) {
+	this->real = rhs.real;
+	this->imaginary = rhs.imaginary;
+
+	return *this;
+}
+
+double Complex::getReal() const {
+	return this->real;
+}
+
+double Complex::getImag() const {
+	return this->imaginary;
+}
+
+double Complex::abs() const {
+	return 0; //sqrt(this->real * this->real + this->imaginary * this->imaginary);
+}
